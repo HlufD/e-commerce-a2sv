806b10ef0ea282f8bf45941849a88cb0e944af99
Hluf Abebe <hlufabebe2015@gmail.com>
2025-11-11 20:49:34 +0300

    intoduce rate limiter

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,30 @@ import { AuthModule } from "./auth/auth.module";
 import { PrismaModule } from "./prisma/prisma.module";
 import { ProductsModule } from "./products/products.module";
 import { OrdersModule } from "./orders/orders.module";
+import { ThrottlerGuard, ThrottlerModule } from "@nestjs/throttler";
+import { APP_GUARD } from "@nestjs/core";
 
 @Module({
-  imports: [AuthModule, PrismaModule, ProductsModule, OrdersModule],
+  imports: [
+    AuthModule,
+    PrismaModule,
+    ProductsModule,
+    OrdersModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 60000,
+          limit: 10,
+        },
+      ],
+    }),
+  ],
   controllers: [AppController],
-  providers: [],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
